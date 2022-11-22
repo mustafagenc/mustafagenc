3217a3ea470a47951718392909bed49b0e7c0a2c
Mustafa Genç <mustafa@ekipisi.com>
2022-11-23 00:59:16 +0300

    nextjs init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: "media", // "class",
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./layouts/**/*.tsx"],
+  safelist: [
+    {
+      pattern: /grid-cols-(2|3|4|5|6)/, // this config for blog post photo grid
+      variants: ["sm", "md", "lg", "xl"],
+    },
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans],
+        serif: ["Tiempos", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
