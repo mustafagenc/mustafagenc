6a79c276c1190d810fd910834fd725750d978e16
Restyled.io <commits@restyled.io>
2022-11-23 22:57:05 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
-const {withContentlayer} = require('next-contentlayer');
+const { withContentlayer } = require('next-contentlayer');
 
 /**
  * @type {import('next').NextConfig}
  */
 
 module.exports = withContentlayer({
-  reactStrictMode : true,
-  swcMinify : true,
-  images : {
-    domains : [
-      'images.unsplash.com', 'dl.airtable.com', 's.gravatar.com',
-      'api.spotify.com', 'accounts.spotify.com', 'i.scdn.co'
-    ]
-  },
-  experimental : {
-    appDir : false,
-    fontLoaders :
-        [ {loader : '@next/font/google', options : {subsets : [ 'latin' ]}} ]
-  }
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: [
+			'images.unsplash.com',
+			'dl.airtable.com',
+			's.gravatar.com',
+			'api.spotify.com',
+			'accounts.spotify.com',
+			'i.scdn.co'
+		]
+	},
+	experimental: {
+		appDir: false,
+		fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }]
+	}
 });
