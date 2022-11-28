3c6d8b2f8cbaeeab81980cb6d21ce359b6be08ff
Mustafa Genç <eposta@mustafagenc.info>
2022-11-28 23:16:46 +0300

    airtable uygulamalar sayfası

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -1,7 +1,21 @@
 import Container from '@/components/container';
 import Title from '@/components/title';
 
+export const revalidate = 86400; // 60*60*24
+
+async function fetchData() {
+	const res = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Apps`, {
+		headers: {
+			Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+		}
+	});
+	return await res.json();
+}
+
 export default async function AppsPage() {
+	const data = await fetchData();
+	console.log(data.records);
+
 	return (
 		<Container>
 			<Title>Uzun süredir kullandığım ve memnun kaldığım uygulamaların listesi.</Title>
