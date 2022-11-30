fcd871967e7810f468de3dfa2cf3d5ebac084f3c
Mustafa Genç <eposta@mustafagenc.info>
2022-11-30 20:57:25 +0300

    vercel düzeltme

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -17,7 +17,7 @@ async function fetchData() {
 export default async function AppsPage() {
 	const data = await fetchData();
 
-	const apps: IApp[] = data.records.map((r) => {
+	const apps: IApp[] = data.records?.map((r) => {
 		return { id: r.id, createdTime: r.createdTime, ...r.fields };
 	});
 
