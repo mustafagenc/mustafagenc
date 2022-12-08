e15ca0431d59200310c53775963bfad77785a130
Mustafa Genç <eposta@mustafagenc.info>
2022-12-09 00:46:44 +0300

    apps ekranı güncellendi

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -1,25 +1,28 @@
 import Apps from '@/components/apps';
 import Container from '@/components/container';
 import Title from '@/components/title';
-import type { IApp } from '@/types/index';
+import { IApp } from '@/types/index';
 
 export const revalidate = 86400; // 60*60*24
 
-async function fetchData() {
+async function getData() {
 	const res = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Apps`, {
 		headers: {
 			Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
 		}
 	});
+
 	return await res.json();
 }
 
 export default async function AppsPage() {
-	const data = await fetchData();
+	const data = (await getData()) || [];
 
-	const apps: IApp[] = data.records?.map((r) => {
-		return { id: r.id, createdTime: r.createdTime, ...r.fields };
-	});
+	const apps: IApp[] =
+		data?.length &&
+		data.records.map((r: any) => {
+			return { id: r.id, createdTime: r.createdTime, ...r.fields };
+		});
 
 	return (
 		<Container>
