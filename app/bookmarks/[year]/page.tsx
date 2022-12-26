3b289cdeabf79cbae96e21987544a4833718dfb4
mustafagenc <eposta@mustafagenc.info>
2022-12-27 01:01:29 +0300

    review

--- app/bookmarks/[year]/page.tsx
+++ app/bookmarks/[year]/page.tsx
@@ -2,25 +2,28 @@ import BookmarkLayout from '@/components/bookmark-layout';
 import bookmarkGroupByWeekNumber from '@/helpers/bookmarkGroupByWeekNumber';
 import Raindrop from '@/helpers/raindrop';
 import { ILink } from '@/types/index';
-import { format, startOfYear } from 'date-fns';
+import { addYears, format, startOfYear } from 'date-fns';
 import { notFound } from 'next/navigation';
 
 export const revalidate = 7200; // 60*60*2
 
 export async function generateStaticParams() {
-	return ['2021', '2022'].map((year) => ({
+	return ['2021', '2022', '2023'].map((year) => ({
 		year: year.toString()
 	}));
 }
 
 async function fetchData(params) {
 	const dateStartOfYear = startOfYear(new Date(params.year));
-	const date = format(dateStartOfYear, 'yyyy-MM-dd');
+	const dateEndOfYear = addYears(dateStartOfYear, 1);
+
+	const startDateByFormat = format(dateStartOfYear, 'yyyy-MM-dd');
+	const endDateByFormat = format(dateEndOfYear, 'yyyy-MM-dd');
 
 	const raindrop = new Raindrop();
 	const collections: ILink[] = await raindrop.multipleRaindrops({
-		id: 29387288,
-		search: `created:>${date}`,
+		id: 15611214,
+		search: `created:>${startDateByFormat} created:<${endDateByFormat}`,
 		allData: true
 	});
 
